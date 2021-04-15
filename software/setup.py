477fb179b3a0cd765565d34714dd78d7ab68f214
Jens Glaser <glaserj@ornl.gov>
2021-04-15 12:55:58 -0400

    make setup.py work

--- software/setup.py
+++ software/setup.py
@@ -2,11 +2,14 @@
 
 from setuptools import setup
 
-setup(name='Deep Boltzmann Package',
+setup(name='deep-boltzmann',
       version='0.1',
       description='Deep Learning Methods for Sampling Boltzmann Distributions',
       author='Frank Noe',
       author_email='frank.noe@fu-berlin.de',
       url='',
-      packages=['deep_boltzmann'],
+      packages=['deep_boltzmann',
+                'deep_boltzmann.models',
+                'deep_boltzmann.networks',
+                'deep_boltzmann.sampling'],
      )
